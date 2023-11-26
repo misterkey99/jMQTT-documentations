ace4e75528202bf5f54beb6858492786cdca786f
Bad <BadWolf42@users.noreply.github.com>
2023-11-26 20:34:45 +0100

    Add ref to this doc

--- js/menu.js
+++ js/menu.js
@@ -1,77 +1,82 @@
 var docMenu = [
     {
-      fr_FR : '<img src="/img/brother_icon.png" style="height:40px;margin-right:13px;" /> Brother',
+        fr_FR: '<img src="/img/brother_icon.png" style="height:40px;margin-right:13px;" /> Brother',
     }, {
-      fr_FR:"Documentation",
-      // en_US:"Documentation",
-      link : "/#LANG#/brother/#VERSION#/index",
-      version: ['stable', 'beta', 'dev'],
-      icon:"fas fa-book",
-      // submenu:[
-      //   {
-      //     fr_FR:"Menu1",
-      //     icon:"fas fa-paint-brush",
-      //     link : "/#LANG#/brother/#VERSION#/m1",
-      //   }, {
-      //     fr_FR:"Menu2",
-      //     icon:"fas fa-cubes",
-      //     link : "/#LANG#/brother/#VERSION#/m2",
-      //   }, {
-      //     fr_FR:"Menu3",
-      //     icon:"fas fa-object-group",
-      //     link : "/#LANG#/brother/#VERSION#/m3",
-      //   }
-      // ]
+        fr_FR: "Documentation",
+        // en_US: "Documentation",
+        link: "/#LANG#/brother/#VERSION#/index",
+        version: ['stable', 'beta', 'dev'],
+        icon: "fas fa-book",
+        // submenu: [
+        //     {
+        //         fr_FR: "Menu1",
+        //         icon: "fas fa-paint-brush",
+        //         link: "/#LANG#/jmqtt/#VERSION#/m1",
+        //     }, {
+        //         fr_FR:"Menu2",
+        //         icon: "fas fa-cubes",
+        //         link: "/#LANG#/jmqtt/#VERSION#/m2",
+        //     }, {
+        //         fr_FR: "Menu3",
+        //         icon: "fas fa-object-group",
+        //         link: "/#LANG#/jmqtt/#VERSION#/m3",
+        //     }
+        // ]
     }, {
-      fr_FR:"Changelog",
-      link : "/#LANG#/brother/changelog",
-      version: ['stable', 'beta', 'dev'],
-      icon:"fas fa-clipboard-list",
+        fr_FR: "Changelog",
+        link : "/#LANG#/brother/changelog",
+        version: ['stable', 'beta', 'dev'],
+        icon: "fas fa-clipboard-list",
     }, {
-      fr_FR:"Github",
-      link : "https://github.com/BadWolf42/plugin-brother",
-      icon:"fas fa-code-branch",
+        fr_FR: "Github",
+        link: "https://github.com/BadWolf42/plugin-brother",
+        icon: "fas fa-code-branch",
     }, {
-      fr_FR:"Community",
-      link : "https://community.jeedom.com/tag/plugin-brother",
-      icon:"fas fa-users",
+        fr_FR: "Community",
+        link: "https://community.jeedom.com/tag/plugin-brother",
+        icon: "fas fa-users",
     }, {
-      divider : true
+        divider: true
     }, {
-      fr_FR : '<img src="/img/jMQTT_icon.png" style="height:40px;margin-right:13px;" /> jMQTT',
+        fr_FR: '<img src="/img/jMQTT_icon.png" style="height:40px;margin-right:13px;" /> jMQTT',
     }, {
-      fr_FR:"Documentation",
-      // en_US:"Documentation",
-      link : "/#LANG#/jmqtt/#VERSION#/index",
-      icon:"fas fa-book",
-      // submenu:[
-      //   {
-      //     fr_FR:"Menu1",
-      //     icon:"fas fa-paint-brush",
-      //     link : "/#LANG#/jmqtt/#VERSION#/m1",
-      //   }, {
-      //     fr_FR:"Menu2",
-      //     icon:"fas fa-cubes",
-      //     link : "/#LANG#/jmqtt/#VERSION#/m2",
-      //   }, {
-      //     fr_FR:"Menu3",
-      //     icon:"fas fa-object-group",
-      //     link : "/#LANG#/jmqtt/#VERSION#/m3",
-      //   }
-      // ]
+        fr_FR: "Documentation",
+        // en_US: "Documentation",
+        link: "/#LANG#/jmqtt/#VERSION#/index",
+        icon: "fas fa-book",
+        // submenu: [
+        //     {
+        //         fr_FR: "Menu1",
+        //         icon: "fas fa-paint-brush",
+        //         link: "/#LANG#/jmqtt/#VERSION#/m1",
+        //     }, {
+        //         fr_FR:"Menu2",
+        //         icon: "fas fa-cubes",
+        //         link: "/#LANG#/jmqtt/#VERSION#/m2",
+        //     }, {
+        //         fr_FR: "Menu3",
+        //         icon: "fas fa-object-group",
+        //         link: "/#LANG#/jmqtt/#VERSION#/m3",
+        //     }
+        // ]
     }, {
-      fr_FR:"Changelog",
-      link : "/#LANG#/jmqtt/#VERSION#/changelog",
-      icon:"fas fa-clipboard-list",
+        fr_FR: "Changelog",
+        link: "/#LANG#/jmqtt/#VERSION#/changelog",
+        icon: "fas fa-clipboard-list",
     }, {
-      fr_FR:"Github",
-      link : "https://github.com/BadWolf42/jMQTT/",
-      icon:"fas fa-code-branch",
+        fr_FR: "Github",
+        link: "https://github.com/BadWolf42/jMQTT/",
+        icon: "fas fa-code-branch",
     }, {
-      fr_FR:"Community",
-      link : "https://community.jeedom.com/tag/plugin-jmqtt",
-      icon:"fas fa-users",
+        fr_FR: "Community",
+        link: "https://community.jeedom.com/tag/plugin-jmqtt",
+        icon: "fas fa-users",
     }, {
-          divider : true
+        divider: true
+    }, {
+        fr_FR: "Modifier cette doc",
+        en_US: "Edit this doc",
+        link: "https://github.com/BadWolf42/documentations/",
+        icon: "fas fa-pen-ruler",
     }
-  ]
+]
