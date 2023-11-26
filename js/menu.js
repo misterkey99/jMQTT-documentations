17328a55dd5fc5279a19aeb233886b3a01d2790f
Bad <BadWolf42@users.noreply.github.com>
2023-11-26 19:38:31 +0100

    Menu menu in another js file

---
+++ js/menu.js
@@ -0,0 +1,61 @@
+var docMenu = [
+    {
+      fr_FR : '<img src="/img/brother_icon.png" style="height:40px;margin-right:13px;" /> Brother',
+    }, {
+      fr_FR:"Documentation",
+      // en_US:"Documentation",
+      link : "/#LANG#/brother/#VERSION#/index",
+      version: ['stable', 'beta', 'dev'],
+      icon:"fas fa-users",
+      // submenu:[
+      //   {
+      //     fr_FR:"Menu1",
+      //     icon:"fas fa-paint-brush",
+      //     link : "/#LANG#/brother/#VERSION#/m1",
+      //   }, {
+      //     fr_FR:"Menu2",
+      //     icon:"fas fa-cubes",
+      //     link : "/#LANG#/brother/#VERSION#/m2",
+      //   }, {
+      //     fr_FR:"Menu3",
+      //     icon:"fas fa-object-group",
+      //     link : "/#LANG#/brother/#VERSION#/m3",
+      //   }
+      // ]
+    }, {
+      fr_FR:"Changelog",
+      link : "/#LANG#/brother/changelog",
+      version: ['stable', 'beta', 'dev'],
+      icon:"fas fa-clipboard-list",
+    }, {
+      divider : true
+    }, {
+      fr_FR : '<img src="/img/jMQTT_icon.png" style="height:40px;margin-right:13px;" /> jMQTT',
+    }, {
+      fr_FR:"Documentation",
+      // en_US:"Documentation",
+      link : "/#LANG#/jmqtt/#VERSION#/index",
+      icon:"fas fa-users",
+      // submenu:[
+      //   {
+      //     fr_FR:"Menu1",
+      //     icon:"fas fa-paint-brush",
+      //     link : "/#LANG#/jmqtt/#VERSION#/m1",
+      //   }, {
+      //     fr_FR:"Menu2",
+      //     icon:"fas fa-cubes",
+      //     link : "/#LANG#/jmqtt/#VERSION#/m2",
+      //   }, {
+      //     fr_FR:"Menu3",
+      //     icon:"fas fa-object-group",
+      //     link : "/#LANG#/jmqtt/#VERSION#/m3",
+      //   }
+      // ]
+    }, {
+      fr_FR:"Changelog",
+      link : "/#LANG#/jmqtt/#VERSION#/changelog",
+      icon:"fas fa-clipboard-list",
+    }, {
+      divider : true
+    }
+  ]
